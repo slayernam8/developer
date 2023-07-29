48a673b75fffc3b61b1dd984cf5d1cf7bc14bd67
slayernam8 <slayernam@gmail.com>
2023-07-29 18:02:39 +0900

    Create ex01-select.sql

---
+++ ex01-select.sql
@@ -0,0 +1,20 @@
+/*
+파일명: ex01-select.sql
+SQL(STRUCTURED QUERY LANGUAGE)- 구조적 질의 언어 
+관계형 데이터 베이스 시스템에서 자료를 관리 및 처리하기 위해 설계된 언어
+SELECT 문
+데이터 베이스에서 정보 검색 명령어
+*/
+-- 모든 열 선택*
+SELECT *
+FROM departments;
+-- 특정열 선택
+SELECT department_id, location_id
+FROM departments;
+
+
+
+
+
+
+
